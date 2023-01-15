51eefff3dc08a1c156e1a108330c19fefbf9b0d8
yicheng55 <l5977951@gmail.com>
2023-01-15 21:55:15 +0800

    config.d.ts added.

---
+++ types/config.d.ts
@@ -0,0 +1,11 @@
+declare global {
+    flds_comp: "abba_dev03",
+    flds_dept: "dept0001",
+    flds_user: "user0001",
+    password: "q45101524Q",
+    location_type: '1100',
+    reserve1: "reserve1",
+    reserve2: "reserve2",
+    reserve3: "reserve3"
+}
+export {}
